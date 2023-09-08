6b502c43810b6e7f44799d8871e7cde72cfb9939
Julien <julien.soule@lcis.grenoble-inp.fr>
2023-09-08 18:48:26 +0200

    added pie chart and histogram in frontend

--- frontend/src/app/agents-histograms/agents-histograms.component.ts
+++ frontend/src/app/agents-histograms/agents-histograms.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { ChartConfiguration } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts'
+import { DataService } from '../services/app.data.service';
 
 @Component({
   selector: 'app-agents-histograms',
@@ -8,72 +10,139 @@ import { ChartConfiguration } from 'chart.js';
 })
 export class AgentsHistogramsComponent implements OnInit {
 
-  teams: any;
+  @ViewChildren(BaseChartDirective) public histograms: QueryList<BaseChartDirective>;
+
+  public teams: any;
+
+  private colorByEp: any[] = [];
+  private lastEpisode: number = -1;
 
   public barChartLegend = true;
   public barChartPlugins = [];
 
-  public barChartData: ChartConfiguration<'bar'>['data'] = {
-    labels: ['action1', 'action2', 'action3', 'action4', 'action5'],
-    datasets: [
-      { data: [4, 9, 17, 47, 6], label: 'Actions' }
-    ]
-  };
-
   public barChartOptions: ChartConfiguration<'bar'>['options'] = {
     responsive: false,
+    plugins: {
+      legend: {
+        display: true,
+      },
+      tooltip: {
+        enabled: true
+      }
+    }
   };
 
-  constructor() {
+  constructor(private dataService: DataService) {
 
     this.teams = {
-      "green": [
-        {
-          "name": "Agt1",
-          "data": {}
-        },
-        {
-          "name": "Agt2",
-          "data": {}
-        },
-        {
-          "name": "Agt3",
-          "data": {}
-        }
-      ],
-      "red": [
-        {
-          "name": "Agt1",
-          "data": {}
-        },
-        {
-          "name": "Agt2",
-          "data": {}
-        },
-        {
-          "name": "Agt3",
-          "data": {}
-        }
-      ],
-      "blue": [
-        {
-          "name": "Agt1",
-          "data": {}
-        },
-        {
-          "name": "Agt2",
-          "data": {}
-        },
-        {
-          "name": "Agt3",
-          "data": {}
-        }
-      ]
+      "green": {},
+      "red": {},
+      "blue": {}
     }
+  }
 
+
+  private pickUniqueRdmCol() {
+    let col = null
+    while (true) {
+      const red = Math.ceil(Math.random() * 256)
+      const green = Math.ceil(Math.random() * 256)
+      const blue = Math.ceil(Math.random() * 256)
+      col = [red, green, blue]
+      if (!this.colorByEp.includes(col)) {
+        break;
+      }
+    }
+    this.colorByEp.push(col)
+    return col
   }
 
   ngOnInit(): void {
+    this.dataService.currentStateData$.subscribe((data) => {
+      if (data !== null && data !== undefined) {
+        if (typeof data == 'object') {
+          if (Object.keys(data).length > 0) {
+            const teamToAgents = data["team_agent_mapping"]
+            const episodeNumber = data["episode_number"]
+            const iterationNumber = data["iteration_number"]
+            const cyborgActions = data["cyborg_actions"]
+            const agentsActions = data["agents_actions"]
+            const cyborgObservations = data["cyborg_observations"]
+            const pz_cyborg_actions = data["pz_cyborg_actions"]
+            const activeAgents = Object.keys(cyborgObservations)
+            if (this.lastEpisode !== episodeNumber) {
+
+              for (const team in teamToAgents) {
+
+                for (const agent of teamToAgents[team]) {
+
+                  if (activeAgents.includes(agent)) {
+
+                    // Adding a new data frame for new episode
+
+                    if (this.teams[team.toLowerCase()][agent] == null || this.teams[team.toLowerCase()][agent] == undefined) {
+                      // If first episode adding all agents
+                      this.teams[team.toLowerCase()][agent] = {
+                        "labels": [],
+                        "datasets": []
+                      }
+                    }
+
+                    const actionNames = []
+                    const actionFrequencies = []
+                    for (const action in pz_cyborg_actions[agent]) {
+                      const actionName = action
+                      actionNames.push(actionName)
+                      actionFrequencies.push(0)
+                    }
+                    this.teams[team.toLowerCase()][agent]["labels"] = actionNames
+
+                    const color = this.pickUniqueRdmCol()
+                    this.teams[team.toLowerCase()][agent]["datasets"].push({
+                      data: actionFrequencies,
+                      label: "Ep. " + episodeNumber,
+                      backgroundColor: 'rgba(' + color[0] + ',' + color[1] + ',' + color[2] + ', 0.2)',
+                      borderColor: 'rgb(' + color[0] + ',' + color[1] + ',' + color[2] + ')',
+                      borderWidth: 1
+                    })
+                  }
+                }
+              }
+
+            } else {
+              // Updating data for current episode
+              for (const team in teamToAgents) {
+                for (const agent of teamToAgents[team]) {
+                  if (activeAgents.includes(agent)) {
+
+                    // Adding a new data frame for new episode
+
+                    const currentActFreqs = this.teams[team.toLowerCase()][agent]["datasets"][episodeNumber].data
+
+                    const playedAction = JSON.stringify(agentsActions[agent])
+                    for (const actionIndex in this.teams[team.toLowerCase()][agent]["labels"]) {
+                      if (playedAction == this.teams[team.toLowerCase()][agent]["labels"][actionIndex]) {
+                        currentActFreqs[actionIndex] = currentActFreqs[actionIndex] + 1
+                        break
+                      }
+                    }
+
+                    this.teams[team.toLowerCase()][agent]["datasets"][episodeNumber].data = currentActFreqs
+                  }
+                }
+              }
+
+            }
+            this.lastEpisode = episodeNumber;
+            this.histograms.toArray().forEach((histogram) => {
+              histogram.chart?.update('none')
+            })
+
+          }
+        }
+      }
+
+    })
   }
 
 }
--- frontend/src/app/agents-piecharts/agents-piecharts.component.ts
+++ frontend/src/app/agents-piecharts/agents-piecharts.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { ChartOptions } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import { DataService } from '../services/app.data.service';
 
 @Component({
   selector: 'app-agents-piecharts',
@@ -8,69 +10,110 @@ import { ChartOptions } from 'chart.js';
 })
 export class AgentsPiechartsComponent implements OnInit {
 
+  @ViewChildren(BaseChartDirective) public piecharts: QueryList<BaseChartDirective>;
+
   teams: any
+  lastEpisode: number = -1;
 
   // Pie
   public pieChartOptions: ChartOptions<'pie'> = {
     responsive: false,
   };
-  public pieChartLabels = [ [ 'Download', 'Sales' ], [ 'In', 'Store', 'Sales' ], 'Mail Sales' ];
-  public pieChartDatasets = [ {
-    data: [ 300, 500, 100 ]
-  } ];
-  public pieChartLegend = true;
+
+  public pieChartLegend = false;
   public pieChartPlugins = [];
 
-  constructor() {
+  constructor(private dataService: DataService) {
 
     this.teams = {
-      "green": [
-        {
-          "name": "Agt1",
-          "data": {}
-        },
-        {
-          "name": "Agt2",
-          "data": {}
-        },
-        {
-          "name": "Agt3",
-          "data": {}
-        }
-      ],
-      "red": [
-        {
-          "name": "Agt1",
-          "data": {}
-        },
-        {
-          "name": "Agt2",
-          "data": {}
-        },
-        {
-          "name": "Agt3",
-          "data": {}
-        }
-      ],
-      "blue": [
-        {
-          "name": "Agt1",
-          "data": {}
-        },
-        {
-          "name": "Agt2",
-          "data": {}
-        },
-        {
-          "name": "Agt3",
-          "data": {}
-        }
-      ]
+      "green": {},
+      "red": {},
+      "blue": {}
     }
 
   }
 
   ngOnInit(): void {
+    this.dataService.currentStateData$.subscribe((data) => {
+      if (data !== null && data !== undefined) {
+        if (typeof data == 'object') {
+          if (Object.keys(data).length > 0) {
+            const teamToAgents = data["team_agent_mapping"]
+            const episodeNumber = data["episode_number"]
+            const iterationNumber = data["iteration_number"]
+            const cyborgActions = data["cyborg_actions"]
+            const agentsActions = data["agents_actions"]
+            const cyborgObservations = data["cyborg_observations"]
+            const pz_cyborg_actions = data["pz_cyborg_actions"]
+            const activeAgents = Object.keys(cyborgObservations)
+            if (this.lastEpisode !== episodeNumber) {
+
+              for (const team in teamToAgents) {
+
+                for (const agent of teamToAgents[team]) {
+
+                  if (activeAgents.includes(agent)) {
+
+                    // Adding a new data frame for new episode
+
+                    if (this.teams[team.toLowerCase()][agent] == null || this.teams[team.toLowerCase()][agent] == undefined) {
+                      // If first episode adding all agents
+                      this.teams[team.toLowerCase()][agent] = {
+                        "labels": [],
+                        "datasets": []
+                      }
+                    }
+
+                    const actionNames = []
+                    const actionFrequencies = []
+                    for (const action in pz_cyborg_actions[agent]) {
+                      const actionName = action
+                      actionNames.push(actionName)
+                      actionFrequencies.push(0)
+                    }
+                    this.teams[team.toLowerCase()][agent]["labels"] = actionNames
+
+                    this.teams[team.toLowerCase()][agent]["datasets"].push({
+                      data: actionFrequencies
+                    })
+                  }
+                }
+              }
+
+            } else {
+              // Updating data for current episode
+              for (const team in teamToAgents) {
+                for (const agent of teamToAgents[team]) {
+                  if (activeAgents.includes(agent)) {
+
+                    // Adding a new data frame for new episode
+
+                    const currentActFreqs = this.teams[team.toLowerCase()][agent]["datasets"][episodeNumber].data
+
+                    const playedAction = JSON.stringify(agentsActions[agent])
+                    for (const actionIndex in this.teams[team.toLowerCase()][agent]["labels"]) {
+                      if (playedAction == this.teams[team.toLowerCase()][agent]["labels"][actionIndex]) {
+                        currentActFreqs[actionIndex] = currentActFreqs[actionIndex] + 1
+                        break
+                      }
+                    }
+
+                    this.teams[team.toLowerCase()][agent]["datasets"][episodeNumber].data = currentActFreqs
+                  }
+                }
+              }
+
+            }
+            this.lastEpisode = episodeNumber;
+            this.piecharts.toArray().forEach((piechart) => {
+              piechart.chart?.update('none')
+            })
+
+          }
+        }
+      }
+
+    })
   }
 
 }
